0db0f56024887a3cb2c6c5f67398d6052b3b9477
Maikel van Leeuwen <maikel@tiny-host.nl>
2018-11-24 19:21:38 +0100

    first push

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # Sport
-Sport related scripts/tutorials/webscraping 
+Sport related scripts/tutorials/web scraping. Several things I automated with python or have been built on top of other python packages, like Garmin Connect Export (https://github.com/pe-st/garmin-connect-export).
+
+The motivation of this repository is simple: Get your data out of the data silos. Exported files or any other content
+should be in an open format with which you can do anything you like.
+
+## Lapscrap
+Lapscrap is a small python script that scrapes the data from mylaps.com and export this to a CSV file. mylaps.com publishes metrics of sport events, but the metrics are not easy to export. The script is 'smart' enough to understand that the tables of most events are slighty different. Be aware that published the exported data in the public is not allowed in Europe due to the new GDPR laws.  
+
+Lapscrap needs several libraries, these can be installed with pip.
+* urllib2
+* BeautifulSoup
+
---
+++ lapscrap/lapscrap.py
@@ -0,0 +1,68 @@
+#!/bin.python
+from __future__ import print_function # use pyton 3 print function for splitting 
+                                      # when we want
+import urllib2
+from bs4 import BeautifulSoup
+import sys
+import os
+
+# Check if argument is given on cli, else exit
+if len(sys.argv) < 2:
+  sys.exit('provide %s "full mylaps.com url"' % sys.argv[0])
+results = sys.argv[1]
+
+directory = "csv"
+agent = ""
+page = urllib2.urlopen(results)
+soup = BeautifulSoup(page, 'html.parser')
+title = soup.find('title')
+title = title.text
+title = title.replace(" ", "_")
+title = title.replace("|", "")
+
+if not os.path.exists(directory):
+  os.makedirs(directory)
+os.chdir(directory)  
+f = open(title+".csv", "w")
+
+list =  []
+headers = []
+participant = 0
+element = 0
+
+for header in soup.find_all('th'):
+    header = header.text
+    header = header.strip()
+    headers.append(header)
+
+count = 0
+for i in headers:
+  count = count + 1
+  print(i, ",", end="", file=f)
+  if count == len(headers):
+        print ("", file=f)
+elementen = len(headers)
+
+for row in soup.find_all('tr'):
+    for col in row.find_all('td'):
+      col = col.text.encode('utf-8').decode('ascii', 'ignore')
+      col = col.strip()   # Strip newline from element
+      col = col.lstrip() 
+      list.append(col)
+
+# First 4 elements of list we need to remove, it is clutter
+del list[0:4]
+list.reverse() # reverse list to delete last elements
+del list[0:4] # Last 4 elements of list we don't need
+list.reverse() # Reverse back, pretty ugly
+
+count = 0
+for i in list:
+  print (i, ",", end="", file=f)  
+  count = count + 1   
+  if count == elementen:
+     participant = participant + 1
+     count = 0
+     print ("", file=f)
+
+f.close
